c0dac4e24f56bd1c3cdef28688ef3e5d028bae19
Harshadeep-M <harshadeep.my@gmail.com>
2026-03-10 18:05:39 +0530

    Add BCrypt passowrd encryption

--- src/main/java/com/readcycle/readcycle/service/UserService.java
+++ src/main/java/com/readcycle/readcycle/service/UserService.java
@@ -8,9 +8,12 @@ import java.util.List;
 import com.readcycle.readcycle.dto.UserDTO;
 import java.util.ArrayList;
 import java.util.List;
-
+import org.springframework.security.crypto.password.PasswordEncoder;
 @Service
 public class UserService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     @Autowired
     private UserRepository userRepository;
     public List<UserDTO> getAllUsers(){
@@ -28,7 +31,8 @@ public class UserService {
         return userDTOList;
     }
     public User createUser(User user){
-        return userRepository.save(user);
+    user.setPassword(passwordEncoder.encode(user.getPassword()));
+    return userRepository.save(user);
     }
     public User getUserById(Long id){
         return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found with id: "+ id));
